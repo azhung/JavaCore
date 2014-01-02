d993d53af15ec00af13837a261640b5a4ef64494
azhung <azhung.it@gmail.com>
2014-01-02 09:16:37 -0800

    This is a lession 6. Will learn Overloading, abstract, extends...

---
+++ Lession_6/InClass/src/demo_access_specifier/pk1/pk2/class2.java
@@ -0,0 +1,17 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package demo_access_specifier.pk1.pk2;
+
+/**
+ *
+ * @author Administrator
+ */
+public class class2 extends demo_access_specifier.pk1.class1 {
+    //lớp con khác gói chỉ được kế thừa thành phần public, protected
+    public class2(){
+      
+    }
+}
---
+++ Lession_6/InClass/src/demo_kethua/Emp_Techical.java
@@ -0,0 +1,21 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package demo_kethua;
+
+/**
+ *
+ * @author Administrator
+ */
+public class Emp_Techical extends Employee {
+    int NgayCong;
+    public Emp_Techical(){
+        super();
+        Luong = 300;
+        NgayCong=0;
+        Hoten ="Abc";
+    }
+
+}
